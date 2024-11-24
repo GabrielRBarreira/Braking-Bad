d7ca1dff84b7cd68b75ffd95f4a0f105b3b2fa33
GabrielRBarreira <barreiragabriel93@gmail.com>
2024-11-24 01:52:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+const header_button = document.querySelector(".header_button");
+const nav = document.querySelector(".header_nav");
+hamburguer.addeventlistener("click", () => nav.classList.toggle("active"));
